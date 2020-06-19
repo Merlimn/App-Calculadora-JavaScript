b645894b0fc8a69f3ac4edc2edafb339558853ec
Merlimn <gabrielfiamete@gmail.com>
2020-06-18 22:27:08 -0300

    change js file

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -11,7 +11,7 @@ function calcular(tipo, valor){
         if( valor === '='){
             var valor_campo = eval(document.getElementById('resultado').value);
             document.getElementById('resultado').value = valor_campo;            
-        }
+        } 
 
     }else if(tipo === 'valor'){       
         document.getElementById('resultado').value +=  valor;
